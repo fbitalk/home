e685c8fdf67830e0ca1a51326aed0d2ca0558871
云之南 <90045224+fbitalk@users.noreply.github.com>
2024-11-01 14:48:07 +0800

    Update Hitokoto.vue

--- src/components/Hitokoto.vue
+++ src/components/Hitokoto.vue
@@ -40,8 +40,8 @@ const openMusicShow = ref(false);
 
 // 一言数据
 const hitokotoData = reactive({
-  text: "这里应该显示一句话",
-  from: "無名",
+  text: "还在写",
+  from: "云之南",
 });
 
 // 获取一言数据
@@ -58,8 +58,8 @@ const getHitokotoData = async () => {
         fill: "#efefef",
       }),
     });
-    hitokotoData.text = "这里应该显示一句话";
-    hitokotoData.from = "無名";
+    hitokotoData.text = "还没有呢";
+    hitokotoData.from = "云之南";
   }
 };
 
